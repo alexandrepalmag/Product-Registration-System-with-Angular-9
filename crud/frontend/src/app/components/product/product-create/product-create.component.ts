f69a73f9db44e2a1c3acc80c162462f5d68398c2
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-09-13 13:57:58 -0300

    Implementation of product registration on the backend

--- crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,15 +1,40 @@
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
+
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  product: Product = {
+    name: 'test product',
+    price: 88.20
+  }
+
+  constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
+
+  }
+
+  createProduct(): void {
+
+    this.productService.create(this.product).subscribe(() => {
+  
+      this.productService.showMessage('Operation successfully executed!')
+      this.router.navigate(['/products'])
+  
+    })
+
+  }
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
