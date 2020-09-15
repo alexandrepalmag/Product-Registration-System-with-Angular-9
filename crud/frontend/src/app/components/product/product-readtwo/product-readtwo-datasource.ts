ed6f726ecd5cbe340099ee3080cce0010417aaab
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-09-14 23:49:49 -0300

    Customizing the table component

--- crud/frontend/src/app/components/product/product-readtwo/product-readtwo-datasource.ts
+++ crud/frontend/src/app/components/product/product-readtwo/product-readtwo-datasource.ts
@@ -3,15 +3,12 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { map } from 'rxjs/operators';
 import { Observable, of as observableOf, merge } from 'rxjs';
+import { Product } from '../product.model';
 
-// TODO: Replace this with your own data model type
-export interface ProductReadtwoItem {
-  name: string;
-  id: number;
-}
+
 
 // TODO: replace this with real data from your application
-const EXAMPLE_DATA: ProductReadtwoItem[] = [
+const EXAMPLE_DATA: Product[] = [
   {id: 1, name: 'Hydrogen'},
   {id: 2, name: 'Helium'},
   {id: 3, name: 'Lithium'},
@@ -39,8 +36,8 @@ const EXAMPLE_DATA: ProductReadtwoItem[] = [
  * encapsulate all logic for fetching and manipulating the displayed data
  * (including sorting, pagination, and filtering).
  */
-export class ProductReadtwoDataSource extends DataSource<ProductReadtwoItem> {
-  data: ProductReadtwoItem[] = EXAMPLE_DATA;
+export class ProductReadtwoDataSource extends DataSource<Product> {
+  data: Product[] = EXAMPLE_DATA;
   paginator: MatPaginator;
   sort: MatSort;
 
@@ -53,7 +50,7 @@ export class ProductReadtwoDataSource extends DataSource<ProductReadtwoItem> {
    * the returned stream emits new items.
    * @returns A stream of the items to be rendered.
    */
-  connect(): Observable<ProductReadtwoItem[]> {
+  connect(): Observable<Product[]> {
     // Combine everything that affects the rendered data into one update
     // stream for the data-table to consume.
     const dataMutations = [
@@ -77,7 +74,7 @@ export class ProductReadtwoDataSource extends DataSource<ProductReadtwoItem> {
    * Paginate the data (client-side). If you're using server-side pagination,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getPagedData(data: ProductReadtwoItem[]) {
+  private getPagedData(data: Product[]) {
     const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
     return data.splice(startIndex, this.paginator.pageSize);
   }
@@ -86,7 +83,7 @@ export class ProductReadtwoDataSource extends DataSource<ProductReadtwoItem> {
    * Sort the data (client-side). If you're using server-side sorting,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getSortedData(data: ProductReadtwoItem[]) {
+  private getSortedData(data: Product[]) {
     if (!this.sort.active || this.sort.direction === '') {
       return data;
     }
--- crud/frontend/src/app/components/product/product-readtwo/product-readtwo.component.ts
+++ crud/frontend/src/app/components/product/product-readtwo/product-readtwo.component.ts
@@ -2,7 +2,8 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
-import { ProductReadtwoDataSource, ProductReadtwoItem } from './product-readtwo-datasource';
+import { ProductReadtwoDataSource } from './product-readtwo-datasource';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-readtwo',
@@ -12,11 +13,11 @@ import { ProductReadtwoDataSource, ProductReadtwoItem } from './product-readtwo-
 export class ProductReadtwoComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatTable) table: MatTable<ProductReadtwoItem>;
+  @ViewChild(MatTable) table: MatTable<Product>;
   dataSource: ProductReadtwoDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['id', 'name', 'price'];
 
   ngOnInit() {
     this.dataSource = new ProductReadtwoDataSource();
